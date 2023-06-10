fc4cdea7255998720f757f7efe3d21ad871d2d3a
Emerson <emerson.day@gmail.com>
2023-06-10 12:06:35 -0700

    updated script

--- insert_data.sh
+++ insert_data.sh
@@ -3,7 +3,7 @@
 # Script to insert data from courses.csv and students.csv into students database
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
-
+echo $($PSQL "TRUNCATE students, majors, courses, majors_courses")
 cat courses_test.csv | while IFS="," read MAJOR COURSE
 do
   if [[ $MAJOR != "major" ]]
@@ -26,14 +26,20 @@ do
     fi
 
     # get course_id
-
+    COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
     # if not found
-
+    if [[ -z $COURSE_ID ]]
+    then
     # insert course
-
+    INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES('$COURSE')")
+    if [[ $INSERT_COURSE_RESULT == 'INSERT 0 1' ]]
+    then 
+      echo Inserted into courses, $COURSE
+    fi
     # get new course_id
 
+    
     # insert into majors_courses
-
+    fi
   fi
 done
